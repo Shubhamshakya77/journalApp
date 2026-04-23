6a0114f9c00c7ea9125bd56f6466b13d96919d92
shubhamshakya77 <shubhamshakya903@gmail.com>
2026-04-23 17:51:11 +0530

    update actions

--- src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
@@ -2,15 +2,18 @@ package net.engineeringdigest.journalApp.service;
 
 import net.engineeringdigest.journalApp.api.Response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+@Service
 public class WeatherService {
-    private static final String apikey = "8182b4790a3303976840da3feddcd806";
+    @Value("${weather.api.key}")
+    private  String apikey;
     private static final String API = "http://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
     @Autowired
